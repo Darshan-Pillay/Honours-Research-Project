d25dd98aff97a7ad691fb291f145239b39343d70
Darshan Pillay <darshan.pillay@takealot.com>
2025-10-25 19:57:12 +0200

    Add functions to serialize and deserialize a model to and from disk

--- Experiment/Utilities/util.py
+++ Experiment/Utilities/util.py
@@ -1,6 +1,9 @@
 import os
 import cv2
 import numpy
+import sklearn
+
+from joblib import dump, load
 
 def create_directory_if_needed(path: str):
     """
@@ -27,3 +30,18 @@ def count_files_in_directory(directory_path: str) -> int:
     for root, _, files in os.walk(directory_path):
         total_files += len(files)
     return total_files
+
+def save_model_to_disk(
+        model: sklearn.svm._classes,
+        destination_path: str
+):
+    """
+    Saves model to disk
+    """
+    dump(model, destination_path)
+
+def load_model_from_disk(model_path: str) -> sklearn.svm._classes:
+    """
+    Loads model from path (assuming model saved using joblib)
+    """
+    return load(model_path)
